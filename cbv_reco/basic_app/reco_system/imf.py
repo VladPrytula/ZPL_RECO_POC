135834ec4251b36de2a6e8879551effec211c885
Vladyslav Prytula <vladyslav.prytula@gmail.com>
2018-09-28 15:56:58 +0200

    1s dirty but working

---
+++ cbv_reco/basic_app/reco_system/imf.py
@@ -0,0 +1,40 @@
+# implicit matrix recommender
+import implicit
+import pickle
+import numpy as np
+from scipy.sparse import load_npz
+import os
+
+
+class ImplicitRecommender():
+    dimentions = 30
+    user_items = load_npz(
+        '/Users/vladyslavp/ML/git.com/Advanced_Django_CBV/reco_poc/cbv_reco/basic_app/reco_system/user_items.npz')
+    print(os.path.dirname(os.path.abspath(__file__)))
+    # duplicated with data loader
+    # shoudl use only os.paths
+    products_arr = np.genfromtxt(
+        "/Users/vladyslavp/ML/git.com/Advanced_Django_CBV/reco_poc/cbv_reco/data/products_arr.csv")
+    customers_arr = np.genfromtxt(
+        "/Users/vladyslavp/ML/git.com/Advanced_Django_CBV/reco_poc/cbv_reco/data/customers_arr.csv")
+
+    def __init__(self):
+        filename = '/Users/vladyslavp/ML/git.com/Advanced_Django_CBV/reco_poc/cbv_reco/basic_app/reco_system/finalized_model.sav'
+        self.loaded_model = pickle.load(open(filename, 'rb'))
+        print('model loaded {}'.format(self.loaded_model))
+
+    def recommend(self, user_id):
+        print('insde the recommend')
+        print(user_id)
+
+        recommendations = self.loaded_model.recommend(
+            user_id, ImplicitRecommender.user_items, N=ImplicitRecommender.dimentions)
+        print(recommendations)
+        fast_recommended = []
+
+        for index in recommendations:
+            code = ImplicitRecommender.products_arr[index[0]]
+            fast_recommended.append(code)
+
+        print("recommened items : {}".format(fast_recommended))
+        return fast_recommended
--- cbv_reco/basic_app/views.py
+++ cbv_reco/basic_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView, ListView, DetailView
 from django.core.paginator import Paginator
-
+from .reco_system.imf import ImplicitRecommender
 from .models import PetOwner, Pet, Product
 
 # Create your views here.
@@ -15,9 +15,25 @@ class PetOwnerListView(ListView):
 
 
 class PetOwnerDetailView(DetailView):
+    imf = ImplicitRecommender()
+    print(imf)
     context_object_name = 'owner_detail'
     model = PetOwner
     template_name = 'basic_app/petowner_detail.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PetOwnerDetailView, self).get_context_data(**kwargs)
+        print(int(context['owner_detail'].lookup_customers_idx))
+
+        context['list_of_recos'] = PetOwnerDetailView.imf.recommend(int(context['owner_detail'].lookup_customers_idx))
+        recommended_items = PetOwnerDetailView.imf.recommend(int(context['owner_detail'].lookup_customers_idx))
+        recommended_products = Product.objects.all().filter(name__in=recommended_items)
+        print(recommended_products)
+        print(context)
+        context['recommended_products'] =recommended_products
+    
+        return context 
 
 
 """ def index(request):
